9dcb081589f1a6060621b2934b057d958dae4f8f
Adam Tomkins <artomkins@googlemail.com>
2018-06-25 17:26:52 +0100

    Add Features and Model

--- UnityProject/Assets/Scenes/RayCast.cs
+++ UnityProject/Assets/Scenes/RayCast.cs
@@ -1,70 +1,369 @@
 ﻿//
 // A Visual Tracker Script to record what the user looks at
 // TODO:
-//      Children objects and Parents
-//      Output the file into a digestable format
+//      Multiple Raycasts  to create a visual field
+//      Record position of camera and direction and length 
+//     
+
+
 
 
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+[System.Serializable]
+public class SerializableDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys = new List<TKey>();
+
+    [SerializeField]
+    private List<TValue> values = new List<TValue>();
+
+    // save the dictionary to lists
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        foreach (KeyValuePair<TKey, TValue> pair in this)
+        {
+            keys.Add(pair.Key);
+            values.Add(pair.Value);
+        }
+    }
+
+    // load dictionary from lists
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+
+        if (keys.Count != values.Count)
+            throw new System.Exception(string.Format("there are {0} keys and {1} values after deserialization. Make sure that both key and value types are serializable."));
+
+        for (int i = 0; i < keys.Count; i++)
+            this.Add(keys[i], values[i]);
+    }
+}
+
+// https://answers.unity.com/questions/460727/how-to-serialize-dictionary-with-unity-serializati.html
+[System.Serializable] public class SDictionary : SerializableDictionary<Vector3, Voxel> { }
+
+
+// Nested Serialisable dicts
+
+[System.Serializable] public class V3Dictionary : SerializableDictionary<string, float> { }
+[System.Serializable] public class V2Dictionary : SerializableDictionary<Vector3, V3Dictionary> { }
+[System.Serializable] public class V1Dictionary : SerializableDictionary<Vector3, V2Dictionary> { }
+
+
+
+
+[System.Serializable]
+public class Voxel
+{
+    public string name;
+
+    public Vector3 position;
+    public float gridInterval;
+    
+    public float viewportGridInterval;
+
+    public int uniqueViews;
+    public float timeViewed;
+    public float lastViewedAt;
+    
+    public GameObject boundryBox;
+}
+
+
+
+
 public class RayCast : MonoBehaviour {
 
     public GameObject marker;
+    public GameObject viewportMarker;
+    public GameObject viewport;
+
     public float gridInterval;
 
+    // Used to record a viewport position at a ganularity
+    public bool trackLocation = true;
+    public float viewportGridInterval;
 
-    Hashtable hashtable;
-    Vector3 previous;
+    public bool exportData = false;
+    public float exportTime = 60.0f;
+    public float timeLimit;
+
+    SDictionary voxelDict;
+    SDictionary viewportVoxelDict;
+
+    V1Dictionary connectionDict;
+
+
+    Voxel currentVoxel;
+    Voxel currentViewportVoxel;
+
+    Vector3 previousIndex;
+    Vector3 previousViewportIndex;
+
+    private float nextActionTime = 0.0f;
+    public float period = 0.1f;
 
     // Use this for initialization
     void Start () {
-         hashtable = new Hashtable();
-         previous = new Vector3(0, 0, 0);
 
+        // A voxel dictionary to store all the voxel viewing information
+        voxelDict = new SDictionary();
+        viewportVoxelDict = new SDictionary();
+        connectionDict = new V1Dictionary();
+
+      
+
+        // Start with a dummy voxel.
+        currentVoxel = new Voxel();
+        previousIndex = new Vector3(0, 0, 0);
+        previousViewportIndex = new Vector3(0, 0, 0);
+
+        // Start with a dummy viewport
+        currentViewportVoxel = new Voxel();
+
+
+        // Scale the marker transform once at the beginning of the run.
         marker.transform.localScale = new Vector3(gridInterval, gridInterval, gridInterval);
 
+        // Scale the viewport marker transform once at the beginning of the run.
+       viewportMarker.transform.localScale = new Vector3(viewportGridInterval, viewportGridInterval, viewportGridInterval);
+
+        // Set up the export timer 
+        timeLimit = exportTime;
+
+    }
+
+
+
+    void ExportData()
+    {
+        string json = JsonUtility.ToJson(voxelDict);
+        System.IO.File.WriteAllText("voxelDict.json", json);
+
+        json = JsonUtility.ToJson(viewportVoxelDict);
+        System.IO.File.WriteAllText("viewportVoxelDict.json", json);
+
+        json = JsonUtility.ToJson(connectionDict);
+        System.IO.File.WriteAllText("connectionDict.json", json);
     }
 	
 	// Update is called once per frame
 	void Update () {
 
-        // Layer 9 is TrackedVisual layer
-        int layerMask = 1 << 9;
-        RaycastHit hit;
-
-        Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 20.0F,layerMask);
-
-        Debug.DrawRay(Camera.main.transform.position, Camera.main.transform.forward);
-
-        if (hit.collider)
+        if (Time.time > nextActionTime)
         {
-             
-            Vector3 vectorFromCenter = gameObject.transform.position - hit.point;
-            Vector3 rotatedVectorFromCenter = Quaternion.Inverse(gameObject.transform.rotation) * vectorFromCenter;
+            nextActionTime += period;
 
-            Vector3 index = new Vector3(rotatedVectorFromCenter[0] - (rotatedVectorFromCenter[0] % gridInterval) + gridInterval/2, rotatedVectorFromCenter[1] - (rotatedVectorFromCenter[1] % gridInterval) + gridInterval / 2, rotatedVectorFromCenter[2] - (rotatedVectorFromCenter[2] % gridInterval) + gridInterval / 2);
+            // Layer 9 is TrackedVisual layer
+            int layerMask = 1 << 9;
+            RaycastHit hit;
+            Physics.Raycast(viewport.transform.position, viewport.transform.forward, out hit, 20.0F, layerMask);
+            Debug.DrawRay(viewport.transform.position, viewport.transform.forward);
 
-            if (hashtable.ContainsKey(index))
+            bool indexChanged = false;
+            bool viewportChanged = false;
+
+            if (hit.collider)
             {
-                hashtable[index] = (int)hashtable[index] +1;
-            } else
-            {
-                hashtable.Add(index, 1);
+                // Calculate Voxel position in unrotated space
+                Vector3 vectorFromCenter = gameObject.transform.position - hit.point;
+                Vector3 rotatedVectorFromCenter = Quaternion.Inverse(gameObject.transform.rotation) * vectorFromCenter;
+                Vector3 index = new Vector3(rotatedVectorFromCenter[0] - (rotatedVectorFromCenter[0] % gridInterval) + gridInterval / 2, rotatedVectorFromCenter[1] - (rotatedVectorFromCenter[1] % gridInterval) + gridInterval / 2, rotatedVectorFromCenter[2] - (rotatedVectorFromCenter[2] % gridInterval) + gridInterval / 2);
 
-                if (marker != null) { Instantiate(marker, index, Quaternion.identity); }
 
-                Debug.DrawLine(previous, index, Color.red, 100.0f);
-                previous = index;
+                // Has the cube changed
+                /*
+                 * Update the time spent at the previous cube 
+                 * 
+                 * Create a new cube if required
+                 */
+
+                if (index != currentVoxel.position)
+                {
+                    indexChanged = true;
+
+                    // Finish previous instance
+                    float elapsedTime = Time.time - currentVoxel.lastViewedAt;
+                    currentVoxel.timeViewed += elapsedTime;
+                    currentVoxel.uniqueViews += 1;
+                    currentVoxel.lastViewedAt = Time.time;
+
+                    // Save results back to the previous Voxel
+                    voxelDict[currentVoxel.position] = currentVoxel;
+
+                    // Retrieve or Start a new instance
+                    if (voxelDict.ContainsKey(index))
+                    {
+                        currentVoxel = voxelDict[index];
+                    }
+                    else
+                    {
+                        currentVoxel = new Voxel();
+                        // set initial values
+                        currentVoxel.name = hit.transform.gameObject.name;
+                        currentVoxel.position = index;
+                        currentVoxel.uniqueViews = 0;
+                        currentVoxel.timeViewed = 0.0f;
+                        currentVoxel.lastViewedAt = Time.time;
+                        if (marker != null)
+                        {
+                            currentVoxel.boundryBox = Instantiate(marker, index, Quaternion.identity);
+                        }
+
+
+                        Debug.DrawLine(previousIndex, index, Color.red, 100.0f);
+
+                        //Export();
+
+                    }
+                }
+
+
+                // Save a copy of the last voxel index for easy look ups
+                previousIndex = index;
+
+                // If we are tracking camera location, we can do the whole shebang again
+
+                if (trackLocation)
+                {
+
+                    if (hit.collider)
+                    {
+                        // Calculate Voxel position in unrotated space
+                        vectorFromCenter = gameObject.transform.position - viewport.transform.position;
+                        rotatedVectorFromCenter = Quaternion.Inverse(gameObject.transform.rotation) * vectorFromCenter;
+                        Vector3 viewportIndex = new Vector3(rotatedVectorFromCenter[0] - (rotatedVectorFromCenter[0] % viewportGridInterval) + viewportGridInterval / 2, rotatedVectorFromCenter[1] - (rotatedVectorFromCenter[1] % viewportGridInterval) + viewportGridInterval / 2, rotatedVectorFromCenter[2] - (rotatedVectorFromCenter[2] % viewportGridInterval) + viewportGridInterval / 2);
+
+
+                        // Has the cube changed
+                        /*
+                         * Update the time spent at the previous cube 
+                         * 
+                         * Create a new cube if required
+                         */
+
+                        if (viewportIndex != currentViewportVoxel.position)
+                        {
+                            viewportChanged = true;
+
+                            // Finish previous instance
+                            float elapsedTime = Time.time - currentViewportVoxel.lastViewedAt;
+                            currentViewportVoxel.timeViewed += elapsedTime;
+                            currentViewportVoxel.uniqueViews += 1;
+                            currentViewportVoxel.lastViewedAt = Time.time;
+
+                            // Save results back to the previous Voxel
+                            viewportVoxelDict[currentViewportVoxel.position] = currentViewportVoxel;
+
+                            // Retrieve or Start a new instance
+                            if (voxelDict.ContainsKey(viewportIndex))
+                            {
+                                currentViewportVoxel = viewportVoxelDict[viewportIndex];
+                            }
+                            else
+                            {
+                                currentViewportVoxel = new Voxel();
+                                // set initial values
+                                currentViewportVoxel.name = "Camera";
+                                currentViewportVoxel.position = viewportIndex;
+                                currentViewportVoxel.uniqueViews = 0;
+                                currentViewportVoxel.timeViewed = 0.0f;
+                                currentViewportVoxel.lastViewedAt = Time.time;
+
+
+                                if (marker != null)
+                                {
+                                    currentViewportVoxel.boundryBox = Instantiate(viewportMarker, viewportIndex, Quaternion.identity);
+                                }
+
+                                Debug.DrawLine(previousViewportIndex, viewportIndex, Color.green, 100.0f);
+
+                            }
+                        }
+
+                        previousViewportIndex = viewportIndex;
+
+                        // A horribly messy solution to record the connections
+                        /* 
+                         * A three depth nested dictionary
+                         * {Vector3  : { Vector3 : { string    : float }}}
+                         * {Object : { Viewport  : { statistic : value }}}
+                         * If either the viewportIndex or the Index has changed, we need to add a connection.
+                         */
+
+                        if (indexChanged || viewportChanged)
+                        {
+                            // Test if the index has already been index
+                            if (!connectionDict.ContainsKey(index))
+                            {
+                                // Create the statistic dictionary
+                                V3Dictionary statDict = new V3Dictionary();
+                                statDict.Add("uniqueViews", 1.0f);
+                                statDict.Add("distance", hit.distance);
+
+                                V2Dictionary viewportDict = new V2Dictionary();
+                                viewportDict.Add(viewportIndex, statDict);
+
+                                connectionDict.Add(index, viewportDict);
+
+                            }
+                            else
+                            {
+                                if (!connectionDict[index].ContainsKey(viewportIndex))
+                                {
+                                    // Create the statistic dictionary
+                                    V3Dictionary statDict = new V3Dictionary();
+                                    statDict.Add("uniqueViews", 1.0f);
+                                    statDict.Add("distance", hit.distance);
+
+                                    V2Dictionary viewportDict = connectionDict[index];
+                                    viewportDict.Add(viewportIndex, statDict);
+                                    connectionDict[index] = viewportDict;
+                                }
+                                else
+                                {
+                                    V3Dictionary statDict = connectionDict[index][viewportIndex];
+                                    statDict["uniqueViews"] += 1.0f;
+                                    statDict["distance"] += hit.distance;
+                                    connectionDict[index][viewportIndex] = statDict;
+                                }
+                            }
+
+                            Debug.DrawLine(index, viewportIndex, Color.blue, 100.0f);
+                        }
+
+                    }
+                }
 
             }
-                
-            
+
+            // translate object for 10 seconds.
+            if (exportData)
+            {
+                // translate object for 10 seconds.
+                if (timeLimit < 0)
+                {
+
+                    Debug.Log("Time to Export");
+                    ExportData();
+                    timeLimit = exportTime;
+                }
+                else
+                {
+                    timeLimit -= Time.deltaTime;
+                }
+            }
         }
     }
-    
 }
-
 
 
---
+++ UnityProject/Assets/Scripts/FPSEcho.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSEcho : MonoBehaviour {
+
+    public float echoTime;
+    public float timeLimit;
+
+    // Use this for initialization
+    void Start () {
+
+        timeLimit = echoTime;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        // translate object for 10 seconds.
+        
+        // translate object for 10 seconds.
+        if (timeLimit < 0)
+        {
+    
+            timeLimit = echoTime;
+            Debug.Log("FPS : " + 1 / Time.deltaTime);
+        }
+        else
+        {
+            timeLimit -= Time.deltaTime;
+        }
+        
+    }
+}
