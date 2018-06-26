d18a0a615b2b9bf0ddae5a20ba99dedd0ffe88d1
Adam Tomkins <artomkins@googlemail.com>
2018-06-26 16:16:39 +0100

    Add Vive Example
    Add a Vive VR scene for visual tracking, and fix several issues to do with mod.

---
+++ VR/VisualTracker VR/Assets/Scripts/Rotate.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public Vector3 RotateAmount;  // degrees per second to rotate in each axis. Set in inspector.
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // RotateAmount = RotateAmount + noise;
+        transform.Rotate(RotateAmount * Time.deltaTime);
+
+    }
+}
---
+++ VR/VisualTracker VR/Assets/Scripts/ViveControllerInput.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViveControllerInput : MonoBehaviour
+{
+    Camera cam;
+    public LayerMask fullMask;
+    public LayerMask partialMask;
+
+    // 1
+    private SteamVR_TrackedObject trackedObj;
+    // 2
+    private SteamVR_Controller.Device Controller
+    {
+        get { return SteamVR_Controller.Input((int)trackedObj.index); }
+    }
+
+    void Awake()
+    {
+        trackedObj = GetComponent<SteamVR_TrackedObject>();
+        cam = Camera.main;
+        fullMask = cam.cullingMask;
+        partialMask = ~LayerMask.NameToLayer("TrackedPoints");
+        cam.cullingMask = partialMask;
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        // 2
+        if (Controller.GetHairTriggerDown())
+        {
+            Debug.Log(gameObject.name + " Trigger Press");
+
+            cam.cullingMask = fullMask;
+        }
+
+        // 3
+        if (Controller.GetHairTriggerUp())
+        {
+            Debug.Log(gameObject.name + " Trigger Release");
+            cam.cullingMask = partialMask;
+        }
+
+    }
+}
